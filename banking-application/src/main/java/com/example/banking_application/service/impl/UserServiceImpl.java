ab6802d8c8d3399a8884ecaca03d870a764c39c1
tanooj2000 <tanooj.sanikey@wipro.com>
2025-09-04 14:04:33 +0530

    commit all changes

---
+++ banking-application/src/main/java/com/example/banking_application/service/impl/UserServiceImpl.java
@@ -0,0 +1,55 @@
+package com.example.banking_application.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.banking_application.dto.UserDto;
+import com.example.banking_application.entity.User;
+import com.example.banking_application.mapper.UserMapper;
+import com.example.banking_application.repository.UserRepository;
+import com.example.banking_application.service.UserService;
+
+@Service
+public class UserServiceImpl implements UserService{
+
+    @Autowired
+    private UserRepository userRepository;
+    
+    @Override
+    public UserDto createUser(UserDto userDto) {
+        User user = UserMapper.mapToUser(userDto);
+        User savedUser = userRepository.save(user);
+        return UserMapper.mapToUserDto(savedUser);
+    }
+
+    @Override
+    public UserDto getUserById(Long id) {
+        User user = userRepository.findById(id).orElse(null);
+        return UserMapper.mapToUserDto(user);
+    }
+
+    @Override
+    public List<UserDto> getAllUsers() {
+        List<User> users = userRepository.findAll();
+        return users.stream().map((user) -> UserMapper.mapToUserDto(user))
+                .toList();
+    }
+
+    @Override
+    public UserDto updateUser(Long id, UserDto userDto) {
+        User user = UserMapper.mapToUser(userDto);
+        user.setId(id);
+        User updatedUser = userRepository.save(user);
+        return UserMapper.mapToUserDto(updatedUser);
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+        User user = userRepository.findById(id).orElse(null);
+        if (user != null) {
+            userRepository.delete(user);
+        }
+    }
+}
