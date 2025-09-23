9cbf4713ad9c3adb6cc9e5075fbd15b15c3c9f38
PinnintiAnusha101 <pinnintianusha.anusha@wipro.com>
2025-09-23 11:58:54 +0530

    commit

--- banking-application/src/main/java/com/example/banking_application/exception/UKBankAccountException.java
+++
@@ -1,7 +0,0 @@
-package com.example.banking_application.exception;
-
-public class UKBankAccountException extends RuntimeException {
-    public UKBankAccountException(String message) {
-        super(message);
-    }
-}
